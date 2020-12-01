dae28877ccce57aab82e10d5ada888e1d8cfd7be
PierreBossard <pbossard.ing2022@esaip.org>
2020-12-01 16:19:46 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'smartwine/addBottle.dart';
 import 'smartwine/rechercher.dart';
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
 import 'smartwine/task.dart';
 import 'smartwine/register.dart';
 import 'smartwine/splash.dart';
@@ -13,10 +11,11 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  //looooool
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Smartwine',
+        title: 'SmartWine',
         theme: ThemeData(
           primarySwatch: Colors.pink,
         ),
